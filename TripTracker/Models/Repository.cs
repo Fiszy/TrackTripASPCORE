764e311fbde19e907b3fc89b6ef5931ebc19ce4a
Akande Afeez <afizakande015@gmail.com>
2019-09-03 11:50:39 +0100

    Initial Commit

---
+++ TripTracker/Models/Repository.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TripTracker.Model;
+
+namespace TripTracker.Models
+{
+    public class Repository
+    {
+        private List<Trip> MyTrips = new List<Trip>
+        {
+            new Trip
+            {
+                Id = 1,
+                Name = "My Summit",
+                StartDate = new DateTime(2019, 3,5),
+                EndDate = new DateTime(2019, 3,8)
+            },
+                        new Trip
+            {
+                Id = 2,
+                Name = "My Journey",
+                StartDate = new DateTime(2019, 4,5),
+                EndDate = new DateTime(2019, 8,8)
+            }
+        };
+
+        public List<Trip> Get()
+        {
+            return MyTrips;
+        }
+
+        public Trip Get(int id)
+        {
+            return MyTrips.First(t => t.Id == id);
+        }
+
+        public void Add(Trip newTrip)
+        {
+            MyTrips.Add(newTrip);
+        }
+
+        public void Update(Trip tripToUpdate)
+        {
+            MyTrips.Remove(MyTrips.First(t => t.Id == tripToUpdate.Id));
+            Add(tripToUpdate);
+        }
+
+        public void Remove(int id)
+        {
+            MyTrips.Remove(MyTrips.First(t => t.Id == id));
+        }
+    }
+}
